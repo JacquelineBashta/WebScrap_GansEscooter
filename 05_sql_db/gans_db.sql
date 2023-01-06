8f45e75eeb4662e5ce41a1da20a25d2618ca0947
JacquelineBashta <jacqueline.gendi@gmail.com>
2023-01-06 15:53:59 +0100

    minor updates after review

--- 05_sql_db/gans_db.sql
+++ 05_sql_db/gans_db.sql
@@ -2,6 +2,7 @@ drop database gans_db;
 create database gans_db;
 use gans_db;
 
+drop table city;
 create table city(
 	city_id int auto_increment,
 	city 		varchar(50),
@@ -12,15 +13,18 @@ create table city(
     primary key (city_id)
 );
 
+
 drop table weather;
 create table weather(
-	city_id 		int ,
+	weather_id 		int auto_increment,
+	city_id 		int not null,
 	forcast_time 	datetime,
 	outlook			varchar(100),
     temperature 	float(3),
 	feels_like 		float(3),
     wind_speed 		float(3),
     rain_prob 		float(3),
+    primary key (weather_id),
     foreign key (city_id) references city(city_id)
 );
 
@@ -28,13 +32,16 @@ drop table airport;
 create table airport(
 	city_id 	int,
 	icao		varchar(10),
+	name		varchar(100),
+    type        varchar(50),
     primary key (icao),
     foreign key (city_id) references city(city_id)
 );
 
+
 drop table flight;
 create table flight(
-	flight_data_id  		int auto_increment,
+	flight_id  				int auto_increment,
 	arrival_icao 			varchar(10),
 	arrival_time_local 		datetime,
 	arrival_terminal		varchar(10),
@@ -44,6 +51,6 @@ create table flight(
     airline 				varchar(50),
     flight_number 			varchar(15),
     data_retrieved_on 		date,
-    primary key (flight_data_id),
+    primary key (flight_id),
     foreign key (arrival_icao) references airport(icao)
 );
